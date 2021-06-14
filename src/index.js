73ca5c7253fc79c829e8860a0327fba7435bbeea
simonojok19 <simonojok19@gmail.com>
2021-06-14 20:02:51 +0300

    added more styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-function Hi() {
-  return (
-      <p>H.</p>
-  );
-}
-render(<Hi />, document.getElementById("app"));
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import  App from './components/App';
+import "./index.css"
+
+render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById("app")
+);
