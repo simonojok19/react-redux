1ad643838d595e8d13b4f7696e1f5347a26a7f11
simonojok19 <simonojok19@gmail.com>
2021-06-14 20:28:57 +0300

    added the header component

---
+++ src/common/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+    const activeStyle = {
+        color: "#f15b2a"
+    }
+    return (
+        <na>
+            <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink> {" | "}
+            <NavLink to="/about" activeStyle={activeStyle} >About</NavLink>
+        </na>
+    )
+}
