de9d0365b2d015287865b764f35dbbccc0fda528
simonojok19 <simonojok19@gmail.com>
2021-06-14 19:54:16 +0300

    added the component content

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default function HomePage() {
+    return (
+        <div className="jumbotron">
+            <h1>Pluralsight Administrations</h1>
+            <p>React, Redux and React Router for ultra-responsive web apps</p>
+            <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+        </div>
+    );
+}
