4f095a492f697ef351f7d1e90c6a8f4c9109d697
simonojok19 <simonojok19@gmail.com>
2021-06-14 20:35:39 +0300

    added nav link for the header component

--- src/common/Header.js
+++ src/common/Header.js
@@ -2,9 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
-    const activeStyle = {
-        color: "#f15b2a"
-    }
+    const activeStyle = { color: "#f15b2a" }
     return (
         <na>
             <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink> {" | "}
